5f8f9157bc3f519c33d8d90c3b216b73d209eb05
Sara Virissimo <sara.bernardov@hotmail.com>
2025-02-05 10:40:57 +0100

    Troca de numero1 e numero2 para multiplicador 1e2

--- multi_float.exemplo.cpp
+++ multi_float.exemplo.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
-float funcao_multiplicacao(float numero1, float numero2);
+float funcao_multiplicacao(float multiplicador1, float multiplicador2);
 
 int main(){
-    float numero1;
-    float numero2;
+    float multiplicador1;
+    float multiplicador2;
 
     std::cout << "escreva o primeiro numero: " << std::endl;
-    std::cin >> numero1;
+    std::cin >> multiplicador1;
     std::cout << "escreva o segundo numero para multiplicar com o primeiro: " << std::endl;
-    std::cin >> numero2;
-    float resultado = funcao_multiplicacao(numero1, numero2);
+    std::cin >> multiplicador2;
+    float resultado = funcao_multiplicacao(multiplicador1, multiplicador2);
     return 0;
 }
 
-float funcao_multiplicacao(float numero1, float numero2) {
-    float resultado = numero1 * numero2;
+float funcao_multiplicacao(float multiplicador1, float multiplicador2) {
+    float resultado = multiplicador1 * multiplicador2;
     std::cout << "resultado: " << resultado << std::endl;
     return resultado;
 }
