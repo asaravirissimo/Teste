ae6d7cea7facd1b867fb54b4d05924a2944a8909
Sara Virissimo <sara.bernardov@hotmail.com>
2025-02-07 15:23:04 +0100

    Adicionei o numero3 para a soma

--- multi_float.exemplo.cpp
+++ multi_float.exemplo.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
-float funcao_multiplicacao(float multiplicador1, float multiplicador2);
+float funcao_multiplicacao(float numero1, float numero2,float numero3);
 
 int main(){
-    float multiplicador1;
-    float multiplicador2;
+    float numero1;
+    float numero2;
+    float numero3;
 
     std::cout << "escreva o primeiro numero: " << std::endl;
-    std::cin >> multiplicador1;
+    std::cin >> numero1;
     std::cout << "escreva o segundo numero para multiplicar com o primeiro: " << std::endl;
-    std::cin >> multiplicador2;
-    float resultado = funcao_multiplicacao(multiplicador1, multiplicador2);
+    std::cin >> numero2;
+    std::cout << "escreva um numero para a soma: " << std::endl;
+    std::cin >> numero3;
+    float resultado = funcao_multiplicacao(numero1, numero2, numero3);
     return 0;
 }
 
-float funcao_multiplicacao(float multiplicador1, float multiplicador2) {
-    float resultado = multiplicador1 * multiplicador2;
+float funcao_multiplicacao(float numero1, float numero2, float numero3) {
+    float resultado = numero1 * numero2 + numero3;
     std::cout << "resultado: " << resultado << std::endl;
     return resultado;
 }
+
